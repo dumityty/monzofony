b27e9ea5860f70eb274ee46bd0af963ead9a8769
dumityty <dumityty@yahoo.com>
2016-11-20 22:01:43 +0000

    fix weird sessions issue

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -88,15 +88,22 @@ class DefaultController extends Controller
         'redirectUri'  => $this->getParameter('redirect_url'),
       ]);
 
+      $session = $request->getSession();
+
       if (!isset($_GET['code'])) {
         // If we don't have an authorization code then get one
         $authUrl = $provider->getAuthorizationUrl();
-        $_SESSION['oauth2state'] = $provider->getState();
+
+        $session->set('oauth2state', $provider->getState());
+
+//        $_SESSION['oauth2state'] = $provider->getState();
+
         header('Location: '.$authUrl);
         exit;
       }
       // Check given state against previously stored one to mitigate CSRF attack
-      elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+      elseif (empty($_GET['state']) || ($_GET['state'] !== $session->get('oauth2state'))) {
+//      elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
         unset($_SESSION['oauth2state']);
         exit('Invalid state');
       } else {

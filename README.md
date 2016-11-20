277d6ef84e54452ebb38179f71ebac792d58fd54
dumityty <dumityty@yahoo.com>
2016-11-20 21:35:41 +0000

    group txns by date; txn page

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # monzofony
 Monzo Dashboard built with the Symfony Framework
 
-I started this project to play with the Monzo API a bit more and to learn Symfony. It is not complete and the code is not in the best shape as I'm constantly learning new stuff and changing it often.
+I started this project to play with the Monzo API a bit more and to get a better understanding of Symfony. It is not complete and the code is not in the best shape as I'm constantly learning new stuff and changing it often.
 
 I have used the following frameworks:
 - [Symfony 3.1](https://symfony.com)
@@ -21,9 +21,28 @@ If anyone wants to set it up, simply
 fork it
 clone it
 run `composer install`
-include these 3 parameters in the parameters.yml file:
+include these parameters in the parameters.yml file:
   client_id: CLIENT_ID
   client_secret: CLIENT_SECRET
   redirect_url: MY_URL/login
+  mapbox_username: MAPBOX_USERNAME
+  mapbox_project_id: MAPBOX_PROJECT_ID
+  mapbox_access_token: MAPBOX_ACCESS_TOKEN
+
 ```
 and it **should** work
+
+I'm using LeafletJS because I always wanted to give it a go and never got around to it. So I'm trying it now. Feel free to change that to something else.
+To use get the mapbox parameters to go [Mapbox](https://www.mapbox.com/) and sign up for an account (it's free).
+
+#### TODO
+- Tidy up collapsed transaction detail on the list of transactions page
+- Map page
+- Provide way to limit amount of transactions by date ranges (last week, last month, last 3 months, all?)
+- Remove any inline css styles I applied in haste :(
+
+#### IN PROGRESS
+- Individual transaction page
+
+#### DONE
+- Basic list of transactions
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -55,17 +55,24 @@ class DefaultController extends Controller
       foreach ($transactions_collection as $txn) {
         $created = $txn->offsetGet('created');
         $date = date('l, j M', strtotime($created));
-        // todo: need to sort the transactions inside the date grouping.
-        $transactions[$date][] = $txn;
+        $transaction_dates[$date][] = $txn;
       }
 
-      $transactions = array_reverse($transactions);
+      // Go through each 'grouped date' and reverse the array
+      // to get the transaction in chronological order.
+      foreach ($transaction_dates as $date => &$transactions) {
+        $transactions = array_reverse($transactions);
+      }
+
+      // Reverse the entire transactions array to be in chronological order.
+      $transaction_dates = array_reverse($transaction_dates);
+
       return $this->render('default/index.html.twig', [
         'account_name' => $description,
         'created' => $created/100,
         'balance' => ($balance/100),
         'spent_today' => ($spent_today/100),
-        'transaction_dates' => $transactions,
+        'transaction_dates' => $transaction_dates,
         'token' => $_SESSION['access_token'],
       ]);
     }
@@ -82,32 +89,32 @@ class DefaultController extends Controller
       ]);
 
       if (!isset($_GET['code'])) {
-          // If we don't have an authorization code then get one
-          $authUrl = $provider->getAuthorizationUrl();
-          $_SESSION['oauth2state'] = $provider->getState();
-          header('Location: '.$authUrl);
-          exit;
-        }
-        // Check given state against previously stored one to mitigate CSRF attack
-        elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-          unset($_SESSION['oauth2state']);
-          exit('Invalid state');
-        } else {
-          // Try to get an access token (using the authorization code grant)
-          $token = $provider->getAccessToken('authorization_code', [
-            'code' => $_GET['code']
-          ]);
+        // If we don't have an authorization code then get one
+        $authUrl = $provider->getAuthorizationUrl();
+        $_SESSION['oauth2state'] = $provider->getState();
+        header('Location: '.$authUrl);
+        exit;
+      }
+      // Check given state against previously stored one to mitigate CSRF attack
+      elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+        unset($_SESSION['oauth2state']);
+        exit('Invalid state');
+      } else {
+        // Try to get an access token (using the authorization code grant)
+        $token = $provider->getAccessToken('authorization_code', [
+          'code' => $_GET['code']
+        ]);
 
-          // Optional: Store the token in the session so we can refresh the page while we're testing
-          $_SESSION['access_token'] = [
-            'access_token' => $token->getToken(),
-            'expires' => $token->getExpires(),
-            'refresh_token' => $token->getRefreshToken(),
-            'resource_owner_id' => $token->getResourceOwnerId()
-          ];
-          header('Location: /');
-          exit;
-        }
+        // Optional: Store the token in the session so we can refresh the page while we're testing
+        $_SESSION['access_token'] = [
+          'access_token' => $token->getToken(),
+          'expires' => $token->getExpires(),
+          'refresh_token' => $token->getRefreshToken(),
+          'resource_owner_id' => $token->getResourceOwnerId()
+        ];
+        header('Location: /');
+        exit;
+      }
     }
 
   /**
